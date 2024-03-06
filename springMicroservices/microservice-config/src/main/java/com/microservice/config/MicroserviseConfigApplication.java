2b7ac5a01d2879c70013d85ac97e3700ba761455
erixrivas <erixrivas@gmail.com>
2024-03-05 20:32:29 -0500

    fix:

---
+++ springMicroservices/microservice-config/src/main/java/com/microservice/config/MicroserviseConfigApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviseConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviseConfigApplication.class, args);
+	}
+
+}
---
+++ springMicroservices/microservice-eureka/src/test/java/com/microservise/eureka/microserviseeureka/MicroserviseEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservise.eureka.microserviseeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviseEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
